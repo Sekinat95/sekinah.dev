8f86aa995d2839479f90eb91ada2ff0d912e2782
sekinat95 <sekinat95@gmail.com>
2020-12-11 18:46:46 +0100

    updated page

--- js/about.js
+++ js/about.js
@@ -1,35 +1,39 @@
 function showNoOfWeeks(){
   const oneWeek =  7 * 24 * 60 * 60 * 1000; //days* hours*minutes*seconds*milliseconds
+  const oneMonth = 4*7 * 24 * 60 * 60 * 1000;
   const firstDate = new Date(2019, 9, 1);
+  const finishDate = new Date(2020, 8, 17);
   const secondDate = Date(Date.now())
-  console.log(secondDate)
+  //console.log(secondDate)
   secondDatee = secondDate.split(" ")
   
   secondDatee.length = 4
   const newSecondDate = formatDate(secondDatee);
-  console.log(newSecondDate)
+ // console.log(newSecondDate)
   const newNewSecondDate = new Date(newSecondDate)
   console.log(newNewSecondDate)
+  console.log(finishDate)
   const diffWeeks = Math.round(Math.abs((firstDate.getTime()- newNewSecondDate.getTime()) / (oneWeek)));
-  return document.getElementById('no-of-weeks').textContent = diffWeeks
+  const finDiffWeeks = Math.floor(Math.abs((finishDate.getTime()- newNewSecondDate.getTime()) / (oneMonth)));
+  return document.getElementById('no-of-weeks').textContent = finDiffWeeks
 }
-function showNoOfDays(){
-  const oneDay =  24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  const firstDate = new Date(2019, 8, 13);
-  const secondDate = Date(Date.now())
-  console.log(secondDate)
-  secondDatee = secondDate.split(" ")
+// function showNoOfDays(){
+//   const oneDay =  24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+//   const firstDate = new Date(2019, 8, 13);
+//   const secondDate = Date(Date.now())
+//   console.log(secondDate)
+//   secondDatee = secondDate.split(" ")
   
-  secondDatee.length = 4
-  const newSecondDate = formatDate(secondDatee);
-  console.log(newSecondDate)
-  const newNewSecondDate = new Date(newSecondDate)
-  console.log(newNewSecondDate)
-  const diffDays = Math.round(Math.abs((firstDate.getTime()- newNewSecondDate.getTime()) / (oneDay)));
-  console.log(diffDays)
-  return document.getElementById('no-of-days').textContent = diffDays
-}
-showNoOfDays()
+//   secondDatee.length = 4
+//   const newSecondDate = formatDate(secondDatee);
+//   console.log(newSecondDate)
+//   const newNewSecondDate = new Date(newSecondDate)
+//   console.log(newNewSecondDate)
+//   const diffDays = Math.round(Math.abs((firstDate.getTime()- newNewSecondDate.getTime()) / (oneDay)));
+//   console.log(diffDays)
+//   return document.getElementById('no-of-days').textContent = diffDays
+// }
+// showNoOfDays()
 showNoOfWeeks()
 
 
